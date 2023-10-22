b230a883502f692f2003b487dea65c1e49ab4d4c
yatume397techcamp <yatume397techcamp@gmail.com>
2023-10-22 09:58:08 +0900

    pageカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_21_231537) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_22_004403) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_231537) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "page"
     t.index ["user_id"], name: "index_worries_on_user_id"
   end
 
