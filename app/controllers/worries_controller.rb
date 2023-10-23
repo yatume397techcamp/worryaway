e3d93037b26bb06a16cf45a01a1728b5677444c5
yatume397techcamp <yatume397techcamp@gmail.com>
2023-10-23 09:48:49 +0900

    投稿したデータを編集する機能

--- app/controllers/worries_controller.rb
+++ app/controllers/worries_controller.rb
@@ -7,11 +7,35 @@ class WorriesController < ApplicationController
 
   def create
     @worry = Worry.new(worry_params)
-
     if @worry.save
       redirect_to user_path(current_user), notice: 'お悩みが投稿されました。'
     else
       render :new
+    end
+  end
+
+  def edit
+    @worry = Worry.find(params[:id])
+  end
+
+  def update
+    @worry = Worry.find(params[:id])
+    if @worry.update(worry_params)
+      redirect_to user_path(current_user), notice: 'お悩みが更新されました。'
+    else
+      render :edit
+    end
+  end
+
+  def edit_selected
+    @selected_worries = Worry.find(params[:selected_worries])
+  
+    # 1つの悩みのみを編集するためのコード
+    if @selected_worries.length == 1
+      redirect_to edit_worry_path(@selected_worries.first)
+    else
+      # ここにエラーメッセージやアラートを表示するロジックを追加できます
+      redirect_to user_path(current_user), alert: '1つの悩みのみを選択してください。'
     end
   end
 
