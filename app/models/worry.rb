b004ecc82009debdc060339a2f4fba5bdb1d99cd
yatume397techcamp <yatume397techcamp@gmail.com>
2023-10-24 22:01:45 +0900

    画像データの透明度対応

--- app/models/worry.rb
+++ app/models/worry.rb
@@ -3,4 +3,9 @@
 class Worry < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  def transparency_level
+    days_passed = (Time.now - updated_at).to_i / 1.day
+    [1 - 0.1 * days_passed, 0.1].max
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# \\wsl.localhost\Ubuntu\home\aivets\projects\worryaway\db\schema.rb
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
